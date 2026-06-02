4db1a25eecde6ea0287820db986de826e04ae256
Smile-HD <erickmiguel2022@hotmail.com>
2026-06-01 22:48:31 -0400

    ciclo4

---
+++ firebase.config.ts
@@ -0,0 +1,12 @@
+// Firebase configuration for Angular Frontend
+export const firebaseConfig = {
+  apiKey: "AIzaSyAvdpjx1YccTHqji2XpwZJX2THnoaXmcOg",
+  authDomain: "asistencia-vehicular-890e2.firebaseapp.com",
+  projectId: "asistencia-vehicular-890e2",
+  storageBucket: "asistencia-vehicular-890e2.firebasestorage.app",
+  messagingSenderId: "770812655534",
+  appId: "1:770812655534:web:66a5392e14282547623a3f"
+};
+
+// VAPID Key - Reemplazar con el key real de Firebase Console
+export const vapidKey = 'BOO39zp0Vpsd10Muf2nntKKpdmegxhs13PuZAMVAgJfrGXDnZdgecRIL2SnqpzAcolXGCWzO9J8xHOafTJwPPmY';
